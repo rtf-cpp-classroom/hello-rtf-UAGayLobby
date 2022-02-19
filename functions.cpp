d38ee8834d83f0a0dbb3d9ce8887e6222bb22c3f
Heorhii Karpynskyi <99879471+UAGayLobby@users.noreply.github.com>
2022-02-19 16:18:58 +0200

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,9 +1,10 @@
 // Please correct the function according to Task.
 
 #include "functions.h"
-
+#include "stdafx.h"
+#include <string>
 
 std::string printHello() 
 {
-	return "hello  xxx";
+	return "hello  RTF";
 }
